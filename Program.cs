6217e7fc5d4820e4eb5aad663ac95ff15e38b5d6
sainani3067 <ammu97938@gmail.com>
2021-09-23 17:51:35 +0530

    Lineofcomparision

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LineofComparisionProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Start.DisplayWelcome();
+            //UC1_LengthOfLineByCoordinates.LengthOfLineByCoordinates();
+            //UC2_CompareEqualityOfLines.CheckEqualityOfLines();
+            //UC3_CompareTwoLines.CompareTwoLines();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
